841404bf584104f83cb9ee722fe0c2d90268a9ee
Nisar Shaikh <>
2021-12-16 12:47:44 +0530

    add tailwind css configurations

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import './App.css';
 
-const App = () => <h1>Hii</h1>;
+const App = () => (
+  <div className="max-w-screen-md mx-auto pt-20">
+    <h1 className="text-3xl text-gray-900 font-bold">
+      Hii, This boiler plate is configured with
+      <ul className="list-disc mt-4 pl-4 ml-4">
+        <li className="text-indigo-600">Eslint</li>
+        <li className="text-yellow-400">Babel</li>
+        <li className="text-rose-800">Prettier</li>
+        <li className="text-sky-400">TailwindCSS</li>
+      </ul>
+    </h1>
+  </div>
+);
 
 export default App;
