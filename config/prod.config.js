a3466c878e10443274e918cfdde24390f8265b1d
Nisar Shaikh <>
2021-12-17 12:37:22 +0530

    adds prod config file

---
+++ config/prod.config.js
@@ -0,0 +1,41 @@
+
+const Dotenv = require('dotenv-webpack');
+const { merge } = require('webpack-merge')
+const base = require('../webpack.config');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = merge(base, {
+  mode: 'production',
+  devtool: false,
+  plugins: [
+    new Dotenv({
+      path: '.env',
+    }),
+    // Extracts CSS into separate files
+    // Note: style-loader is for development, MiniCssExtractPlugin is for production
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].[contenthash].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  module: {
+    rules: [],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      `...`,
+      new TerserPlugin(),
+      new CssMinimizerPlugin(),
+    ],
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+})
