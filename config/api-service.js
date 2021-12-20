b1cb3d3d5d61eb56bc72b7a274798aeef4a62712
Nisar Shaikh <>
2021-12-20 11:36:43 +0530

    adds pai endpoint

--- config/api-service.js
+++ config/api-service.js
@@ -1,17 +1,18 @@
 import axios from 'axios';
 
 export const fetchApi = ({ apiUrl, apiMethod = 'GET', payload = null, flag = false, token }) => {
-  const endPoint = `${process.env.API_URL}${apiUrl}`;
+  const API_URL = 'https://contract-management-node-api.herokuapp.com';
+  const endPoint = `${API_URL}${apiUrl}`;
   const header = flag
     ? {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': window.location.origin,
-        Authorization: `bearer ${token}`
-      }
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': window.location.origin,
+      Authorization: `bearer ${token}`
+    }
     : {
-        'Access-Control-Allow-Origin': window.location.origin,
-        'Content-Type': 'application/json'
-      };
+      'Access-Control-Allow-Origin': window.location.origin,
+      'Content-Type': 'application/json'
+    };
 
   return axios({
     url: endPoint,
